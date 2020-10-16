35a727e2098033db0aa3b883adcf6a901617e875
unknown <jennimar17@gmail.com>
2020-10-16 05:22:43 -0300

    agrego funcionalidad de abrir y cerrar cesta con js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const botonVistaComprimida = document.getElementById("button-view-zip")
 const botonVistaExpandida = document.getElementById("button-view-expand")
 const seccionTarjetasComprimidas = document.querySelector(".products-cards-section")
-console.log(seccionTarjetasComprimidas)
 const seccionTarjetasExpandidas = document.querySelector(".products--cards--section")
 
 seccionTarjetasExpandidas.classList.add("ocultar")
@@ -15,4 +14,18 @@ botonVistaExpandida.onclick = () => {
 botonVistaComprimida.onclick = () => {
     seccionTarjetasExpandidas.classList.add("ocultar")
     seccionTarjetasComprimidas.classList.remove("ocultar")
+}
+
+const seccionCestaLlena = document.querySelector(".items-cest-container")
+const botonAbrirCesta = document.querySelector(".shopping-basket-section")
+const botonCerrarCesta = document.getElementById("button-close-cest")
+
+seccionCestaLlena.classList.add("ocultar")
+
+botonAbrirCesta.onclick = () => {
+    seccionCestaLlena.classList.remove("ocultar")
+}
+
+botonCerrarCesta.onclick = () => {
+    seccionCestaLlena.classList.add("ocultar")
 }
